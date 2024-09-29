fe2b015bfe8aee11ba56dcf82f3936a8956bda6c
Eirik Stanghelle Morland <eirik@morland.no>
2024-09-29 07:14:16 +0200

    Put raw count in txt

--- update.sh
+++ update.sh
@@ -28,6 +28,7 @@ while IFS= read -r line; do
     image=$(echo "$line" | cut -d'/' -f3)
     raw_pulls=$(curl -sSLN https://github.com/"$owner"/"$repo"/pkgs/container/"$image" | grep -Pzo '(?<=Total downloads</span>\n          <h3 title=")\d*')
     pulls=$(curl -sSLN https://github.com/"$owner"/"$repo"/pkgs/container/"$image" | grep -Pzo "(?<=Total downloads</span>\n          <h3 title=\"$raw_pulls\">)[^<]*")
+    echo $pulls > $owner-$repo.txt
     date=$(date -u +"%Y-%m-%d")
 
     if [ -n "$pulls" ]; then
